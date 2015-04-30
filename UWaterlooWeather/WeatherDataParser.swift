25f0e70265966be37ad7b145d9837b010ab6bffd
Daniel Johnson <dan@ddajohnson.com>
2015-04-30 13:16:09 -0400

    Improved error handling when failing to download data (no longer crashes)

--- UWaterlooWeather/WeatherDataParser.swift
+++ UWaterlooWeather/WeatherDataParser.swift
@@ -30,25 +30,32 @@ class WeatherDataParser : NSObject {
             
             //println(xmlData)
             let weatherData = WeatherObservation(xmlData:xmlData);
-            println("UW Weather Data acquired")
             
             //Download weather condition data from the OpenWeatherMap API.
-            
-            let weatherCondTask = NSURLSession.sharedSession().dataTaskWithURL(NSURL(string:"http://api.openweathermap.org/data/2.5/weather?id=6176823&mode=xml&APPID=03346813fdb0dc3c37b3c96518f067f3")!) {(data, response, error) in
+            if weatherData != nil {
+                println("UW Weather Data acquired")
+                let weatherCondTask = NSURLSession.sharedSession().dataTaskWithURL(NSURL(string:"http://api.openweathermap.org/data/2.5/weather?id=6176823&mode=xml&APPID=03346813fdb0dc3c37b3c96518f067f3")!) {(data, response, error) in
+                    
+                    let xmlData = SWXMLHash.parse(data)
+                    
+                    weatherData!.weatherConditionCode = xmlData["current"]["weather"].element?.attributes["number"]?.trimAndToInt()
+                    
+                    dispatch_async(dispatch_get_main_queue(),{
+                        //Post a notification that new weather data is now available on the main thread.
+                        println("Weather Data notification posted")
+                        let weatherNotification = NSNotification(name: Constants.Notifications.kWeatherDataAvailable, object: weatherData)
+                        NSNotificationCenter.defaultCenter().postNotification(weatherNotification)
+                    
+                    })
+                }
+                weatherCondTask.resume()
+            } else {
+                println("UW Weather Data failed to acquire")
+                //Post Notification
                 
-                let xmlData = SWXMLHash.parse(data)
-                
-                weatherData.weatherConditionCode = xmlData["current"]["weather"].element?.attributes["number"]?.trimAndToInt()
-                
-                dispatch_async(dispatch_get_main_queue(),{
-                    //Post a notification that new weather data is now available on the main thread.
-                    println("Weather Data notification posted")
-                    let weatherNotification = NSNotification(name: Constants.Notifications.kWeatherDataAvailable, object: weatherData)
-                    NSNotificationCenter.defaultCenter().postNotification(weatherNotification)
-                
-                })
+                //Try again?
+                               
             }
-            weatherCondTask.resume()
         }
         
         task.resume()
@@ -60,7 +67,7 @@ class WeatherDataParser : NSObject {
 
 
 
-class WeatherObservation {
+class WeatherObservation : NSObject {
     var dataSource: String?
     var dataSourceURL: NSURL?
     var locationName: String?
@@ -93,7 +100,7 @@ class WeatherObservation {
     // We will either get it from another source, or infer it.
     var weatherConditionCode: Int?
     
-    init () {
+    override init () {
         let currentDate = NSDate()
         let calendar = NSCalendar.currentCalendar()
         let components = calendar.components(.CalendarUnitYear | .CalendarUnitMonth | .CalendarUnitDay | .CalendarUnitHour | .CalendarUnitMinute, fromDate:currentDate)
@@ -107,40 +114,46 @@ class WeatherObservation {
         observationDay = components.day
         observationHour = components.hour
         observationMinute = components.minute
+        super.init()
     }
     
-    init (xmlData: XMLIndexer) {
-        let currentObs = xmlData["current_observation"]
-        dataSource = currentObs["credit"].element!.text!.trim()
-        let urlString = currentObs["credit_URL"].element!.text!.trim()
-        dataSourceURL = NSURL(string:urlString!)
-        locationName = currentObs["location"].element!.text!.trim()
-        latitude = currentObs["latitude"].element!.text!.trim()
-        longitude = currentObs["longitude"].element!.text!.trim()
-        elevation = currentObs["elevation"].element!.text!.trim()
-        observationMonthName = currentObs["observation_month_text"].element!.text!.trim()
-        observationMonthNumber = currentObs["observation_month_number"].element!.text!.trimAndToInt()
-        observationDay = currentObs["observation_day"].element!.text!.trimAndToInt()
-        observationYear = currentObs["observation_year"].element!.text!.trimAndToInt()
-        observationHour = currentObs["observation_hour"].element!.text!.trimAndToInt()
-        observationMinute = currentObs["observation_minute"].element!.text!.trimAndToInt()
-        temperatureCelcius = currentObs["temperature_current_C"].element!.text!.trimAndToFloat()
-        humidexCelcius = currentObs["humidex_C"].element!.text!.trimAndToFloat()
-        windchillCelcius = currentObs["windchill_C"].element!.text!.trimAndToFloat()
-        temperature24HrMax = currentObs["temperature_24hrmax_C"].element!.text!.trimAndToFloat()
-        temperature24HrMin = currentObs["temperature_24hrmin_C"].element!.text!.trimAndToFloat()
-        precipitation15Min_mm = currentObs["precipitation_15minutes_mm"].element!.text!.trimAndToFloat()
-        precipitation1Hr_mm = currentObs["precipitation_1hr_mm"].element!.text!.trimAndToFloat()
-        precipitation24Hr_mm = currentObs["precipitation_24hr_mm"].element!.text!.trimAndToFloat()
-        relativeHumidityPercent = currentObs["relative_humidity_percent"].element!.text!.trimAndToFloat()
-        dewpointCelcius = currentObs["dew_point_C"].element!.text!.trimAndToFloat()
-        windspeedKph = currentObs["wind_speed_kph"].element!.text!.trimAndToFloat()
-        windDirection = currentObs["wind_direction"].element!.text!.trim()
-        windDirectionDegrees = currentObs["wind_direction_degrees"].element!.text!.trimAndToInt()
-        pressureKpa = currentObs["pressure_kpa"].element!.text!.trimAndToFloat()
-        pressureTrend = currentObs["pressure_trend"].element!.text!.trim()
-        shortwaveRadiationWpM2 = currentObs["incoming_shortwave_radiation_WM2"].element!.text!.trimAndToFloat()
-       
+    init? (xmlData: XMLIndexer) {
+        super.init()
+        if (xmlData.children.count != 0) {
+            let currentObs = xmlData["current_observation"]
+            dataSource = currentObs["credit"].element!.text!.trim()
+            let urlString = currentObs["credit_URL"].element!.text!.trim()
+            dataSourceURL = NSURL(string:urlString!)
+            locationName = currentObs["location"].element!.text!.trim()
+            latitude = currentObs["latitude"].element!.text!.trim()
+            longitude = currentObs["longitude"].element!.text!.trim()
+            elevation = currentObs["elevation"].element!.text!.trim()
+            observationMonthName = currentObs["observation_month_text"].element!.text!.trim()
+            observationMonthNumber = currentObs["observation_month_number"].element!.text!.trimAndToInt()
+            observationDay = currentObs["observation_day"].element!.text!.trimAndToInt()
+            observationYear = currentObs["observation_year"].element!.text!.trimAndToInt()
+            observationHour = currentObs["observation_hour"].element!.text!.trimAndToInt()
+            observationMinute = currentObs["observation_minute"].element!.text!.trimAndToInt()
+            temperatureCelcius = currentObs["temperature_current_C"].element!.text!.trimAndToFloat()
+            humidexCelcius = currentObs["humidex_C"].element!.text!.trimAndToFloat()
+            windchillCelcius = currentObs["windchill_C"].element!.text!.trimAndToFloat()
+            temperature24HrMax = currentObs["temperature_24hrmax_C"].element!.text!.trimAndToFloat()
+            temperature24HrMin = currentObs["temperature_24hrmin_C"].element!.text!.trimAndToFloat()
+            precipitation15Min_mm = currentObs["precipitation_15minutes_mm"].element!.text!.trimAndToFloat()
+            precipitation1Hr_mm = currentObs["precipitation_1hr_mm"].element!.text!.trimAndToFloat()
+            precipitation24Hr_mm = currentObs["precipitation_24hr_mm"].element!.text!.trimAndToFloat()
+            relativeHumidityPercent = currentObs["relative_humidity_percent"].element!.text!.trimAndToFloat()
+            dewpointCelcius = currentObs["dew_point_C"].element!.text!.trimAndToFloat()
+            windspeedKph = currentObs["wind_speed_kph"].element!.text!.trimAndToFloat()
+            windDirection = currentObs["wind_direction"].element!.text!.trim()
+            windDirectionDegrees = currentObs["wind_direction_degrees"].element!.text!.trimAndToInt()
+            pressureKpa = currentObs["pressure_kpa"].element!.text!.trimAndToFloat()
+            pressureTrend = currentObs["pressure_trend"].element!.text!.trim()
+            shortwaveRadiationWpM2 = currentObs["incoming_shortwave_radiation_WM2"].element!.text!.trimAndToFloat()
+        } else {
+            println("Weather Data initialization failed")
+            return nil
+        }
     }
     
 /*    func inferWeatherCondition() -> String {
